0300d2f6211ac95c34dd4e6d0b916df1cff18255
FloofHips <61808556+FloofHips@users.noreply.github.com>
2021-10-19 16:53:34 +0100

    Refactoring and vanilla+ update

--- src/main/java/floofhips/brickandbrack/datagen/LanguageGen.java
+++ src/main/java/floofhips/brickandbrack/datagen/LanguageGen.java
@@ -23,12 +23,27 @@ public class LanguageGen extends LanguageProvider
         this.addBlock(BlockInit.LARGE_CHAINS, "Large Chains");
         this.addBlock(BlockInit.LATTICE_BLOCK, "Lattice Block");
         this.addBlock(BlockInit.PEBBLED_DIRT, "Pebbled Dirt");
-        this.addBlock(BlockInit.ROOFING_BLOCK, "Roofind Block");
-        this.addBlock(BlockInit.ROOFING_SLAB, "Roofing Slab");
-        this.addBlock(BlockInit.ROOFING_STAIRS, "Roofing Stairs");
+        this.addBlock(BlockInit.CLAY_ROOFING_BLOCK, "Clay Roofind Block");
+        this.addBlock(BlockInit.CLAY_ROOFING_SLAB, "Clay Roofing Slab");
+        this.addBlock(BlockInit.CLAY_ROOFING_STAIRS, "Clay Roofing Stairs");
+        this.addBlock(BlockInit.CLAY_TILE_BLOCK, "Clay Tile Block");
+        this.addBlock(BlockInit.SLATE_ROOFING_BLOCK, "Slate Roofind Block");
+        this.addBlock(BlockInit.SLATE_ROOFING_SLAB, "Slate Roofing Slab");
+        this.addBlock(BlockInit.SLATE_ROOFING_STAIRS, "Slate Roofing Stairs");
+        this.addBlock(BlockInit.SLATE_TILE_BLOCK, "Slate Tile Block");
         this.addBlock(BlockInit.SHRUB, "Shrub");
-        this.addBlock(BlockInit.TILE_BLOCK, "Tile Block");
         this.addBlock(BlockInit.OAK_POLE, "Oak Pole");
+        this.addBlock(BlockInit.DIORITE_BRICKS, "Diorite Bricks");
+        this.addBlock(BlockInit.DIORITE_TILES, "Diorite Tiles");
+        this.addBlock(BlockInit.ANDESITE_TILES , "Andesite Bricks");
+        this.addBlock(BlockInit.ANDESITE_BRICKS, "Andesite Tiles");
+        this.addBlock(BlockInit.GRANITE_BRICKS, "Granite Bricks");
+        this.addBlock(BlockInit.GRANITE_TILES, "Granite Tiles");
+        this.addBlock(BlockInit.BASALT_BRICKS, "Basalt Bricks");
+        this.addBlock(BlockInit.BASALT_TILES, "Basalt Tiles");
+        this.addBlock(BlockInit.OBSIDIAN_BRICKS, "Obsidian Bricks");
+        this.addBlock(BlockInit.CHISELED_ENDSTONE, "Chiseled Endstone");
+        this.addBlock(BlockInit.CHISELED_PRISMARINE, "Chiseled Prismarine");
 
         this.add("itemGroup.BrickAndBrack", "Brick & Brack");
     }
--- src/main/java/floofhips/brickandbrack/init/BlockInit.java
+++ src/main/java/floofhips/brickandbrack/init/BlockInit.java
@@ -12,35 +12,51 @@ import java.util.function.Supplier;
 
 public class BlockInit {
 
-    // Roofing
-
-    public static final RegistryObject<Block> ROOFING_BLOCK = register("roofing_block", () -> new HorizontalConnectingBlock(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(3).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops().sound(SoundType.NETHER_BRICKS)));
-	public static final RegistryObject<Block> ROOFING_SLAB = register("roofing_slab", () -> new HorizontalConnectingSlab(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(3).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops().sound(SoundType.NETHER_BRICKS)));
-	public static final RegistryObject<Block> ROOFING_STAIRS = register("roofing_stairs", () -> new HorizontalConnectingStairs(() -> Blocks.AIR.defaultBlockState(), AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(3).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops().sound(SoundType.NETHER_BRICKS)));
-    public static final RegistryObject<Block> TILE_BLOCK = register("tile_block", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(3).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops().sound(SoundType.NETHER_BRICKS)));
-
-    // Plants
-
-    public static final RegistryObject<Block> BUSH_BLOCK = register("bush_block", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(0.2F).sound(SoundType.SWEET_BERRY_BUSH).noOcclusion()));
-    public static final RegistryObject<Block> SHRUB = register("shrub", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(0.2F).sound(SoundType.SWEET_BERRY_BUSH).noOcclusion()));
-
-    // LightSources
-
-    // Panes
-
-    public static final RegistryObject<Block> LATTICE_BLOCK = register("lattice_block", () -> new PaneBlock(AbstractBlock.Properties.of(Material.DECORATION).strength(2,2).harvestLevel(0).harvestTool(ToolType.AXE).requiresCorrectToolForDrops().sound(SoundType.SCAFFOLDING)));
-
-    //Vanilla+ (Block Variations)
+    // The Vanilla+ Update (Block Variations)
 
     public static final RegistryObject<Block> PEBBLED_DIRT = register("pebbled_dirt", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(0).harvestTool(ToolType.SHOVEL).sound(SoundType.BASALT)));
     public static final RegistryObject<Block> DIRTY_PEBBLES = register("dirty_pebbles", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(0).harvestTool(ToolType.SHOVEL).sound(SoundType.NETHER_GOLD_ORE)));
+    public static final RegistryObject<Block> OAK_POLE = register("oak_pole", () -> new ConnectingPillarBlock(AbstractBlock.Properties.of(Material.DECORATION).strength(2).harvestTool(ToolType.AXE).sound(SoundType.WOOD)));
+    public static final RegistryObject<Block> DIORITE_BRICKS = register("diorite_bricks", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> GRANITE_BRICKS = register("granite_bricks", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> ANDESITE_BRICKS = register("andesite_bricks", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> BASALT_BRICKS = register("basalt_bricks", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> DIORITE_TILES = register("diorite_tiles", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> GRANITE_TILES = register("granite_tiles", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> ANDESITE_TILES = register("andesite_tiles", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> BASALT_TILES = register("basalt_tiles", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> CHISELED_ENDSTONE = register("chiseled_endstone", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> CHISELED_PRISMARINE = register("chiseled_prismarine", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> OBSIDIAN_BRICKS = register("obsidian_bricks", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)));
 
-    //Connectors
+    // The Home Improvement Update
+
+    public static final RegistryObject<Block> SLATE_ROOFING_BLOCK = register("slate_roofing_block", () -> new HorizontalConnectingBlock(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(3).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops().sound(SoundType.NETHER_BRICKS)));
+	public static final RegistryObject<Block> SLATE_ROOFING_SLAB = register("slate_roofing_slab", () -> new HorizontalConnectingSlab(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(3).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops().sound(SoundType.NETHER_BRICKS)));
+	public static final RegistryObject<Block> SLATE_ROOFING_STAIRS = register("slate_roofing_stairs", () -> new HorizontalConnectingStairs(() -> Blocks.AIR.defaultBlockState(), AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(3).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops().sound(SoundType.NETHER_BRICKS)));
+    public static final RegistryObject<Block> SLATE_TILE_BLOCK = register("slate_tile_block", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(3).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops().sound(SoundType.NETHER_BRICKS)));
+    public static final RegistryObject<Block> CLAY_ROOFING_BLOCK = register("clay_roofing_block", () -> new HorizontalConnectingBlock(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(3).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops().sound(SoundType.NETHER_BRICKS)));
+    public static final RegistryObject<Block> CLAY_ROOFING_SLAB = register("clay_roofing_slab", () -> new HorizontalConnectingSlab(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(3).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops().sound(SoundType.NETHER_BRICKS)));
+    public static final RegistryObject<Block> CLAY_ROOFING_STAIRS = register("clay_roofing_stairs", () -> new HorizontalConnectingStairs(() -> Blocks.AIR.defaultBlockState(), AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(3).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops().sound(SoundType.NETHER_BRICKS)));
+    public static final RegistryObject<Block> CLAY_TILE_BLOCK = register("clay_tile_block", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(5, 11).harvestLevel(3).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops().sound(SoundType.NETHER_BRICKS)));
+
+    // The Dungeon Update
 
     public static final RegistryObject<Block> LARGE_CHAINS = register("large_chains", () -> new ChainsBlock(AbstractBlock.Properties.of(Material.DECORATION).strength(2).harvestTool(ToolType.AXE).sound(SoundType.WOOD)));
-    public static final RegistryObject<Block> OAK_POLE = register("oak_pole", () -> new ConnectingPillarBlock(AbstractBlock.Properties.of(Material.DECORATION).strength(2).harvestTool(ToolType.AXE).sound(SoundType.WOOD)));
 
-    //Special Blocks
+    // The Green Update
+
+    public static final RegistryObject<Block> BUSH_BLOCK = register("bush_block", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(0.2F).sound(SoundType.SWEET_BERRY_BUSH).noOcclusion()));
+    public static final RegistryObject<Block> SHRUB = register("shrub", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).strength(0.2F).sound(SoundType.SWEET_BERRY_BUSH).noOcclusion()));
+    public static final RegistryObject<Block> LATTICE_BLOCK = register("lattice_block", () -> new PaneBlock(AbstractBlock.Properties.of(Material.DECORATION).strength(2,2).harvestLevel(0).harvestTool(ToolType.AXE).requiresCorrectToolForDrops().sound(SoundType.SCAFFOLDING)));
+
+    // The Factory Update
+
+    // The Cozy Wozy Update
+
+    // The Urban Update
+
+    //registers
 
     private static <T extends Block>RegistryObject<T> registerNoItem(String name, Supplier<T> block) {
         return InitContent.BLOCKS.register(name, block);
